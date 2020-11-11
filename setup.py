eeecbb2946c5c08a7785f29d23e7c3d6890994f4
tsutsu3 <tsutsu3prog@gmail.com>
2020-11-11 19:17:18 +0900

    Fix docs

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ with open(path.join(path.dirname(__file__), "uc_micro", "__init__.py")) as f:
     match = re.search(r"__version__\s*=\s*[\'\"](.+?)[\'\"]", f.read())
     version = match.group(1)
 
-
 setuptools.setup(
     name="uc-micro-py",
     version=version,
@@ -22,11 +21,12 @@ setuptools.setup(
     url="https://github.com/tsutsu3/uc.micro-py",
     packages=setuptools.find_packages(exclude=["test"]),
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
